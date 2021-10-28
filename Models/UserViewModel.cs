dd5a1361fddfc396f89613042bff74c5188c30e4
RonniSouza <ronnisouza21@gmail.com>
2021-10-27 22:44:52 -0300

    Propriedades do usuario e tela de login

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -1,11 +1,11 @@
-namespace squadmanager.Models
+namespace SquadManager.Models
 {
     public class UserViewModel
     {
-        public string Username { get; set;}
+        public string Username { get; set; }
 
-        public string Email {get; set;}
+        public string Email { get; set; }
 
-        public string Password {get; set;}
+        public string Password { get; set; }
     }
 }
