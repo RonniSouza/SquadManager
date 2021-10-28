b8ddc4ceb095d9b802542b12da97c8af77343b2b
RonniSouza <ronnisouza21@gmail.com>
2021-10-27 21:59:37 -0300

    estrutura inicial

---
+++ Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+namespace squadmanager.Models
+{
+    public class UserViewModel
+    {
+        public string Username { get; set;}
+
+        public string Email {get; set;}
+
+        public string Password {get; set;}
+    }
+}
